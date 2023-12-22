f691a5ee59316fb4a8a3f2c1798c36f203b68838
deepkhunt54 <deep.k@simformsolutions.com>
2023-12-22 16:55:23 +0530

    graphql practice code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { typeDefs } from './schemas/schema.js'
+import { resolvers } from './resolver/resolver.js'
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+})
+
+console.log(`Server is listening on PORT: ${url}`)
